ead580bd8e5292233f4b848fa70ac8da54b6b3a6
Agnes <koinageagness@gmail.com>
2022-04-18 02:12:52 +0300

    add directive highlight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubFormComponent } from './github-form/github-form.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubFormComponent
+    GithubFormComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,

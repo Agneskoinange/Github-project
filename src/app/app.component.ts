9532c9abddd0d84830790c8983fa0c306b3799e6
Agnes <koinageagness@gmail.com>
2022-04-18 01:57:11 +0300

    add comonent github-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-// import { OnDestroy, OnInit } from '@angular/core';
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 // import { Subscription } from 'rxjs';
-// import { APIService } from './services/api.service';
+import { APIService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Github-project';
+
+
+  
+    }
   
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubFormComponent } from './github-form/github-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubFormComponent
   ],
   imports: [
     BrowserModule,

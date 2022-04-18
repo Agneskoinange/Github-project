e4dfbc555822c853321867691a1eb1e8fd38ac05
Agnes <koinageagness@gmail.com>
2022-04-19 01:36:55 +0300

    modify app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { APIService } from './services/api.service';
+import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'Github-project';
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+  })
+  
+export class AppComponent implements OnInit {
+title = 'Github-project';
+
+  user: any= [];
+  repos: any= [];
+  username: string | undefined;
+  constructor(private apiservice: APIService) {
+    this.apiservice.getUser().subscribe((user: any) => {
+      console.log(user);
+      this.user = user;
+    });
+    this.apiservice.getRepos().subscribe((repos: any) => {
+     
+      this.repos = repos;
+    });
+  }
+  ngOnInit() {
+  }
+   searchUser() {
+     this.apiservice.updateUser(this.username);
+     this.apiservice.getUser().subscribe((user: any) => {
+       this.user = user;
+     });
+     this.apiservice.getRepos().subscribe((repos: any) => {
+      
+       this.repos = repos;
+     });
+
+   }
 }

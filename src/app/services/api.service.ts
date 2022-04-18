83da6b37a3b63c9fb037c5748606a110f58680a3
Agnes <koinageagness@gmail.com>
2022-04-18 03:36:43 +0300

    import userand repos from user and repository class to API service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,11 +1,36 @@
-// import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { UserClass } from '../user-class';
+import { RepositoryClass } from '../repository-class';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class APIService {
+
+  constructor(private http: HttpClient) {
+    console.log('Github Service Ready...');
+    this.username =  'agneskoinange';
+
+  }
+  getUserClass() {
+    return this.http.get('https://api.github.com/users/' + this.username)
+    .map(result => result);
+
+  }
+  get RepositoryClass() {
+    return this.http.get(' https://api.github.com/users/' + this.username + '/repos')
+    .map(result => result);
+  }
+
+  updateRepo(username: string) {
+    this.username = username;
+  }
+
+}
 
 //   constructor(private http:HttpClient) { }
    
@@ -15,4 +40,3 @@ export class APIService {
 //   getRepo(username:string){
 //     return this.http.get(`https:github.com/users/agneskoinange/repos`);
 //   }
-}
